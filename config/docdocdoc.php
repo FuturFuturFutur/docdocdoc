223c20c532c878a9f6f4d23c8d58d453f12e561c
Mushu <Mushu@inernetbs.net>
2021-03-23 08:15:43 +0200

    Create docdocdoc configuration

---
+++ config/docdocdoc.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return [
+    'mode_on' => env('DOCDOCDOC_MODE_ON', false),
+    'type' => 'swagger',
+    'title' => 'Name of the API documentation',
+    'description' => 'Description of the API',
+    'version' => '1.0.0',
+];
--- src/DocdocdocServiceProvider.php
+++ src/DocdocdocServiceProvider.php
@@ -2,6 +2,13 @@
 
 namespace Futurfuturfutur\Docdocdoc;
 
+use Futurfuturfutur\Docdocdoc\Commands\InitCommand;
+use Futurfuturfutur\Docdocdoc\Commands\RenderCommand;
+use Futurfuturfutur\Docdocdoc\Http\Middleware\DocDocDocMiddleware;
+use Futurfuturfutur\Docdocdoc\Services\Format\FormatServiceInterface;
+use Futurfuturfutur\Docdocdoc\Services\Format\SwaggerFormatService;
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class DocdocdocServiceProvider extends ServiceProvider
